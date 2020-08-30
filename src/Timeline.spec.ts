de11e14340bbc28567971de5ae2962d9ed5ab41f
marcio.v.ordonez@gmail.com <marcio.v.ordonez@gmail.com>
2020-08-30 15:53:31 -0300

    Timeline component and frist tests

---
+++ src/Timeline.spec.ts
@@ -0,0 +1,31 @@
+import { mount } from '@vue/test-utils';
+
+import Timeline from './Timeline.vue';
+
+
+describe('Timeline',()=>{
+    it('Renders 3 time periods',()=>{
+        const wrapper = mount(Timeline);
+
+        expect(wrapper.findAll('[data-test="period"]')).toHaveLength(3);
+    });
+
+    it('Updates the period when clicked',async ()=>{
+        const wrapper = mount(Timeline);
+        const today = wrapper.findAll('[data-test="period"]')[0];
+        expect(today.classes()).toContain('is-active');
+
+        const thisWeek = wrapper.findAll('[data-test="period"]')[1];
+        await thisWeek.trigger('click');
+
+        expect(today.classes()).not.toContain('is-active');
+        expect(thisWeek.classes()).toContain('is-active');
+
+        const thisMonth = wrapper.findAll('[data-test="period"]')[2];
+        await thisMonth.trigger('click');
+
+        expect(today.classes()).not.toContain('is-active');
+        expect(thisWeek.classes()).not.toContain('is-active');
+        expect(thisMonth.classes()).toContain('is-active');
+    });
+})
