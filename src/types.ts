93679577bcc654d2ec6c9665b7e69604add30cc4
marcio.v.ordonez@gmail.com <marcio.v.ordonez@gmail.com>
2020-08-30 18:08:48 -0300

    using cumputed and testing it

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,12 @@
-export type Period = 'today' | 'this week' | 'this month';
+import { Moment } from "moment";
+
+export type Period = 'today' | 'this week' | 'this month'
+
+export interface Post {
+  id: number
+  title: string
+  markdown: string
+  html: string
+  authorId: number
+  created: Moment
+}
